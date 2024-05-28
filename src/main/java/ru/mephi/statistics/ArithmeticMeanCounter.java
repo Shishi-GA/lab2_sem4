a4e962452e2169c809feb96bd6e82db32f120403
polina <shishigapochta@gmail.com>
2024-05-28 21:06:05 +0300

    initial commit

---
+++ src/main/java/ru/mephi/statistics/ArithmeticMeanCounter.java
@@ -0,0 +1,24 @@
+package ru.mephi.statistics;
+
+import org.apache.commons.math3.stat.StatUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+//среднее арифметическое(2)
+public class ArithmeticMeanCounter implements StatisticsCounter {
+    private List<Double> result = new ArrayList<>();
+
+    @Override
+    public void calculate(List<List<Double>> columns) {
+        result = new ArrayList<>();
+        for (List<Double> column : columns) {
+            double mean = StatUtils.mean(column.stream().mapToDouble(Double::doubleValue).toArray());
+            result.add(mean);
+        }
+    }
+
+    @Override
+    public List<Double> getResult() {
+        return result;
+    }
+}
